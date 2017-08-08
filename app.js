bf56842355f7dc9a7dd0c32a9c9db7aa39c90f21
Emma Dean <emmadeannow@gmail.com>
2017-08-08 14:05:53 -0400

    fixed header issue, hanging on selecting robot individually

--- app.js
+++ app.js
@@ -1,14 +1,17 @@
 const express = require('express');
 const expressHandlebars = require('express-handlebars');
-const dataFile = require('./public/data')
-const fs = require('fs');
+const dataFile = require('./public/data');
+const bodyParser = require('body-parser')
 const app = express()
 
 app.engine('handlebars', expressHandlebars());
 app.set('views', './views');
 app.set('view engine', 'handlebars');
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 
+app.use(express.static('public'));
 
 app.get('/', function(req, res){
 //for loop if value key is null, target same place and assign it to string "availble for hire" console.log(dataFile)
@@ -21,15 +24,19 @@ for (var i = 0; i < dataFile.users.length; i++) {
   res.render('home', dataFile)
 });
 
-app.get("/user/:username", function(req, res){
-  let usernameLink = req.params.name;
 
+app.get("/user/:username", function(req, res){
+  let robot = req.params.username;
   for (var i = 0; i < dataFile.users.length; i++) {
-      res.render('user', dataFile.users[i])
+    if(dataFile.users[i].username === robot){
+     res.render('user', dataFile.users[i]);
+    }
   }
+
+
 });
 
-app.use(express.static('public'));
+
 
 
 app.listen(3000);
